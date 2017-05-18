e2b2b786d7f89c64be17e82ab2c735ecda6ddb3f
ymmtmdk <ymmtmdk@gmail.com>
2017-05-18 19:53:38 +0900

    Revert "no comment"
    This reverts commit aa24d1e58f492eb41cd4a5023c5dd8c786e3116d.

--- client/pitcher.ts
+++ client/pitcher.ts
@@ -2,17 +2,17 @@ import Complex from './complex';
 
 class Pitcher{
   private static parabola(nsdf: Array<number>, i:number){
-    const a:number = nsdf[i-1];
-    const b:number = nsdf[i];
-    const c:number = nsdf[i+1];
+    var a:number = nsdf[i-1];
+    var b:number = nsdf[i];
+    var c:number = nsdf[i+1];
 
-    const bottom:number = a + c - 2.0 * b;
-    let x:number, y:number;
-    if (bottom === 0.0){
+    var bottom:number = a + c - 2.0 * b;
+    var x:number, y:number;
+    if (bottom == 0.0){
       x = i;
       y = b;
     }else{
-      const delta = a - c;
+      var delta = a - c;
       x = i + delta / (2.0 * bottom);
       y = b - delta * delta / (8.0 * bottom);
     }
@@ -53,13 +53,13 @@ class Pitcher{
     var peaks = new Array<number>();
     for (let i = 0; i < nsdf.length-1; i++){
       if (nsdf[i] > 0.0){
-	if (nega && (maxI===0 || nsdf[i]>nsdf[maxI])) maxI = i;
+        if (nega && (maxI==0 || nsdf[i]>nsdf[maxI])) maxI = i;
       }else{
-	if (maxI>0){
-	  peaks.push(maxI);
-	  maxI = 0;
-	}
-	nega = true;
+        if (maxI>0){
+          peaks.push(maxI);
+          maxI = 0;
+        }
+        nega = true;
       }
     }
     if (maxI>0) peaks.push(maxI);
@@ -70,14 +70,13 @@ class Pitcher{
   static pitch(ary: Array<number>, sampleRate: number){
     const DEFAULT_CUTOFF = 0.95;
 
-    // const x2: Array<Complex> = ary.map((e)=>{return new Complex(e, 0)});
     const x = new Array<Complex>();
     for (let i = 0; i < ary.length; i++) x[i] = new Complex(ary[i], 0);
 
     const nsdf = this.nsdf(x);
 
     const peaks = this.picking(nsdf);
-    if (peaks.length === 0) return -1.0;
+    if (peaks.length == 0) return -1.0;
 
     const periods = new Array<number>();
     const amps = new Array<number>();
@@ -88,20 +87,27 @@ class Pitcher{
       periods.push(h.x);
     }
 
-    const max = amps.reduce((a,b)=>Math.max(a,b));
+    const max = Lambda.fold(amps, function(e, max){return e > max ? e : max;}, 0.0);
 
     if (max < 0.35) return -1.0;
     const coff = DEFAULT_CUTOFF * max;
 
-    let idx = amps.find(e=> e > coff);-1;
-    if (idx === undefined) return -1.0;
+    let idx = -1;
+    for (let i = 0; i < amps.length; i++){
+      if (amps[i] > coff){
+        idx = i;
+        break;
+      }
+    }
+
+    if (idx == -1) return -1.0;
 
     return sampleRate / periods[idx];
   }
 }
 
 class FFT{
-  private static fft_ = function(n: number, s: number, eo: boolean, x: Array<Complex>, y: Array<Complex>) {
+  static fft_ = function(n,s,eo,x,y) {
     var m = Math.floor(n / 2);
     var theta = 2.0 * Math.PI / n;
     if(n == 1) {
@@ -120,8 +126,8 @@ class FFT{
 	var _g11 = 0;
 	while(_g11 < s) {
 	  var q1 = _g11++;
-	  var a: Complex = x[q1 + s * p];
-	  var b: Complex = x[q1 + s * (p + m)];
+	  var a = x[q1 + s * p];
+	  var b = x[q1 + s * (p + m)];
 	  y[q1 + s * (2 * p)] = a.plus(b);
 	  y[q1 + s * (2 * p + 1)] = a.minus_bang(b).multi(wp);
 	}
@@ -130,10 +136,36 @@ class FFT{
     }
   }
 
-  static fft = function(x: Array<Complex>, n: number) {
-    FFT.fft_(n,1,false,x,new Array<Complex>());
+  static fft = function(x,n) {
+    FFT.fft_(n,1,false,x,new Array());
   }
 }
+
+class HxOverrides{
+  static iter(a) {
+    return { cur : 0, arr : a, hasNext : function() {
+      return this.cur < this.arr.length;
+    }, next : function() {
+      return this.arr[this.cur++];
+    }};
+  }
+}
+
+class Lambda{
+  static fold(it,f,first) {
+    var $it0 = $iterator(it)();
+    while( $it0.hasNext() ) {
+      var x = $it0.next();
+      first = f(x,first);
+    }
+    return first;
+  }
+}
+
+function $iterator(o) { if( o instanceof Array ) return function() { return HxOverrides.iter(o); }; return typeof(o.iterator) == 'function' ? $bind(o,o.iterator) : o.iterator; }
+var $_, $fid = 0;
+
+function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
 
 export default Pitcher;
 
