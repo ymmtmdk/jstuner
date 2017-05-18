209cce5a482a9df77e268ea0e3b888faa56a6627
ymmtmdk <ymmtmdk@gmail.com>
2017-05-18 20:32:34 +0900

    no comment

--- client/pitcher.ts
+++ client/pitcher.ts
@@ -98,25 +98,32 @@ class Pitcher{
 
 class FFT{
   private static fft_inner(n: number,stride: number,copy_flag: boolean,x: Array<Complex>,y: Array<Complex>) {
-    if(n === 1) {
-      if(copy_flag) {
-        for (let q = 0; q < stride; q++){ y[q] = x[q]; }
+    while (true){
+      if(n === 1) {
+        if(copy_flag) {
+          for (let q = 0; q < stride; q++){ y[q] = x[q]; }
+        }
+        return;
       }
-      return;
-    }
 
-    const m = Math.floor(n / 2);
-    const theta = 2.0 * Math.PI / n;
-    for (let p = 0; p < m; p++){
-      const wp = new Complex(Math.cos(p * theta),-Math.sin(p * theta));
-      for (let q = 0; q < stride; q++){
-        const a = x[q + stride * p];
-        const b = x[q + stride * (p + m)];
-        y[q + stride * (2 * p)] = a.plus(b);
-        y[q + stride * (2 * p + 1)] = a.minus_bang(b).multi(wp);
+      const m = Math.floor(n / 2);
+      const theta = 2.0 * Math.PI / n;
+      for (let p = 0; p < m; p++){
+        const wp = new Complex(Math.cos(p * theta),-Math.sin(p * theta));
+        for (let q = 0; q < stride; q++){
+          const a = x[q + stride * p];
+          const b = x[q + stride * (p + m)];
+          y[q + stride * (2 * p)] = a.plus(b);
+          y[q + stride * (2 * p + 1)] = a.minus_bang(b).multi(wp);
+        }
       }
+      n /= 2;
+      stride *= 2;
+      copy_flag = !copy_flag;
+      [x, y] = [y, x];
+
+      // FFT.fft_inner(n / 2, 2 * stride, !copy_flag, y, x);
     }
-    FFT.fft_inner(n / 2, 2 * stride, !copy_flag, y, x);
   }
 
   static fft(x: Array<Complex>, n: number) {
