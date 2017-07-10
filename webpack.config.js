ffefdb098023ec553e1dfe31d1c2497e12d27437
ymmtmdk <ymmtmdk@gmail.com>
2017-07-10 14:44:45 +0900

    no comment

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: "./client/main.ts",
+  entry: "./src/main.ts",
   output: {
     path: path.resolve(__dirname, 'public/assets'),
     filename: "bundle.js"
