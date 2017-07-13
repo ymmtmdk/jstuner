d6b06abc0b4ace595042b2d6d9f24e3da0f27839
ymmtmdk <ymmtmdk@gmail.com>
2017-07-13 13:54:13 +0900

    no comment

--- src/main.ts
+++ src/main.ts
@@ -1,114 +1,119 @@
 import { Note, Pitcher } from 'pitch-detector';
 
-let audioContext, canvas, canvasContext;
-audioContext = null;
-canvas = null;
-canvasContext = null;
 
-function setPixel(imageData, x: number, y: number, color) {
-  const width = imageData.width;
-  const data = imageData.data;
-  const index = ((width * y) + x) * 4;
-  if (!isNaN(color.r)) {
-    data[index] = color.r;
-  }
-  if (!isNaN(color.g)) {
-    data[index + 1] = color.g;
-  }
-  if (!isNaN(color.b)) {
-    data[index + 2] = color.b;
-  }
-  if (!isNaN(color.a)) {
-    return data[index + 3] = color.a;
-  }
-};
+class JsTuner{
+  audioContext;
+  canvas;
+  canvasContext;
 
-function drawWave(buffer, note: Note) {
-  let x, y;
-  canvasContext.save();
-  canvasContext.fillStyle = "rgb(30, 30, 30)";
-  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-  canvasContext.restore();
-  const imageData = canvasContext.getImageData(0, 0, canvas.width, canvas.height);
-  const color = {
-    r: 200,
-    g: 200,
-    b: 200,
-    a: 255
+  constructor(){
+    this.canvas = document.getElementById("wave");
+    this.canvasContext = this.canvas.getContext("2d");
+    this.audioContext = new AudioContext();
+  }
+
+  setPixel(imageData, x: number, y: number, color) {
+    const width = imageData.width;
+    const data = imageData.data;
+    const index = ((width * y) + x) * 4;
+    if (!isNaN(color.r)) {
+      data[index] = color.r;
+    }
+    if (!isNaN(color.g)) {
+      data[index + 1] = color.g;
+    }
+    if (!isNaN(color.b)) {
+      data[index + 2] = color.b;
+    }
+    if (!isNaN(color.a)) {
+      return data[index + 3] = color.a;
+    }
+  }
+
+  drawWave(buffer, note: Note) {
+    let x, y;
+    this.canvasContext.save();
+    this.canvasContext.fillStyle = "rgb(30, 30, 30)";
+    this.canvasContext.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.canvasContext.restore();
+    const imageData = this.canvasContext.getImageData(0, 0, this.canvas.width, this.canvas.height);
+    const color = {
+      r: 200,
+      g: 200,
+      b: 200,
+      a: 255
+    };
+    const red = {
+      r: 200,
+      g: 0,
+      b: 0,
+      a: 255
+    };
+
+    const width = imageData.width;
+    const height = imageData.height;
+    for (let x = 0; x < width; x++){
+      y = Math.floor(height/2+buffer[x*2]*height)
+      this.setPixel(imageData, x, y, color);
+    }
+
+    x = Math.round(width/2 + width * note.diff())
+    for (let y = 0; y < height; y++){
+      this.setPixel(imageData, x, y, color);
+      this.setPixel(imageData, width/2, y, red);
+    }
+    this.canvasContext.putImageData(imageData, 0, 0);
   };
-  const red = {
-    r: 200,
-    g: 0,
-    b: 0,
-    a: 255
+
+  connectRecorder(stream) {
+    const hzElement = document.getElementById("hz");
+    const noteElement = document.getElementById("note");
+    const bufferSize = 2048;
+    const recorder = this.audioContext.createScriptProcessor(bufferSize, 2, 2);
+    let counter = 0;
+    recorder.onaudioprocess = (e)=> {
+      const span = document.hasFocus() ? 2 : 16;
+      if (counter++ % span != 0) {
+        return;
+      }
+      const left = e.inputBuffer.getChannelData(0);
+      const hz = Pitcher.pitch(left, this.audioContext.sampleRate);
+      const note = new Note(hz);
+      this.drawWave(left, note);
+      if (!(hz >= 30)) {
+        return;
+      }
+      hzElement.innerHTML = 'hz = ' + hz;
+      noteElement.innerHTML = 'note = ' + note.name();
+    };
+    const input = this.audioContext.createMediaStreamSource(stream);
+    input.connect(recorder);
+    return recorder.connect(this.audioContext.destination);
   };
 
-  const width = imageData.width;
-  const height = imageData.height;
-  for (let x = 0; x < width; x++){
-    y = Math.floor(height/2+buffer[x*2]*height)
-    setPixel(imageData, x, y, color);
-  }
+  main(){
+    const nav: any = navigator;
+    const win: any = window;
 
-  x = Math.round(width/2 + width * note.diff())
-  for (let y = 0; y < height; y++){
-    setPixel(imageData, x, y, color);
-    setPixel(imageData, width/2, y, red);
-  }
-  canvasContext.putImageData(imageData, 0, 0);
-};
-
-function connectRecorder(stream) {
-  audioContext = new AudioContext();
-  const hzElement = document.getElementById("hz");
-  const noteElement = document.getElementById("note");
-  const bufferSize = 2048;
-  const recorder = audioContext.createScriptProcessor(bufferSize, 2, 2);
-  let counter = 0;
-  recorder.onaudioprocess = function(e) {
-    const span = document.hasFocus() ? 2 : 16;
-    if (counter++ % span != 0) {
+    if (!nav.getUserMedia) {
+      nav.getUserMedia = nav.getUserMedia || nav.webkitGetUserMedia || nav.mozGetUserMedia || nav.msGetUserMedia;
+    }
+    if (!win.AudioContext) {
+      win.AudioContext = win.webkitAudioContext;
+    }
+    if (!nav.getUserMedia || !win.AudioContext) {
+      alert("not supported in this browser.");
       return;
     }
-    const left = e.inputBuffer.getChannelData(0);
-    const hz = Pitcher.pitch(left, audioContext.sampleRate);
-    const note = new Note(hz);
-    drawWave(left, note);
-    if (!(hz >= 30)) {
-      return;
-    }
-    hzElement.innerHTML = 'hz = ' + hz;
-    noteElement.innerHTML = 'note = ' + note.name();
-  };
-  const input = audioContext.createMediaStreamSource(stream);
-  input.connect(recorder);
-  return recorder.connect(audioContext.destination);
-};
 
-function main(){
-  const nav: any = navigator;
-  const win: any = window;
-
-  if (!nav.getUserMedia) {
-    nav.getUserMedia = nav.getUserMedia || nav.webkitGetUserMedia || nav.mozGetUserMedia || nav.msGetUserMedia;
+    nav.getUserMedia(
+      { audio: true },
+      this.connectRecorder.bind(this), () => alert("error capturing audio.")
+    );
   }
-  if (!win.AudioContext) {
-    win.AudioContext = win.webkitAudioContext;
-  }
-  if (!nav.getUserMedia || !win.AudioContext) {
-    alert("not supported in this browser.");
-    return;
-  }
-
-  nav.getUserMedia(
-    { audio: true },
-    connectRecorder, ()=> {alert("error capturing audio.");}
-  );
-  canvas = document.getElementById("wave");
-  canvasContext = canvas.getContext("2d");
 }
 
 window.onload = () => {
-  main();
+  (new JsTuner()).main();
 };
 
